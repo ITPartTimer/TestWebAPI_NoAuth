2adb1391ac0a51fe54b2f7c152f1b25cb357fc76
Scott <cscottierun@gmail.com>
2017-01-22 22:34:59 -0500

    Started OPC controllers

---
+++ TestWebAPI_NoAuth/Controllers/OPCController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebAPI_NoAuth.Repositories;
+using TestWebAPI_NoAuth.Models;
+
+namespace TestWebAPI_NoAuth.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/opc")]
+    public class OPCController : ApiController
+    {
+        OPCRepository _repo = new OPCRepository();
+
+        public OPCController()
+        {
+
+        }
+
+        //Need async methods for api/opc/add and api/opc/add/tail
+    }
+}
